dc7e1a36c77ccdac0da2ed23b8a36bffc489e00b
ashleynicole472 <asha.4720@gmail.com>
2018-07-16 12:11:45 -0700

    Working on quick links, created carousel and styled container

--- fitfoodie-recipesearch/src/Components/QuickLinkCarousel/QuickLinks.js
+++ fitfoodie-recipesearch/src/Components/QuickLinkCarousel/QuickLinks.js
@@ -1,11 +1,94 @@
 import React, { Component } from 'react';
+import samplepic from './QuickLinksImages/recipes.png';
 import './QuickLinks.css';
 
 class QuickLinks extends Component {
     render(){
         return(
-            <div>
-                <h1>Within Random Recipes Squares</h1>
+            <div className="quicklinks-container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1 className="quick-links-h1">Quick Recipe search</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div id="carousel">
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                            <div className="slide-pic">
+                                <a href="#">
+                                    <img src={samplepic} className="pic" alt="example" /><br />
+                                    <p className="category-title">Chicken</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
