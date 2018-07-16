77faee833a6754dc9b56df9b10ed21d8230da86d
ashleynicole472 <asha.4720@gmail.com>
2018-07-16 12:24:23 -0700

    Styling quick links

--- fitfoodie-recipesearch/src/Components/QuickLinkCarousel/QuickLinks.js
+++ fitfoodie-recipesearch/src/Components/QuickLinkCarousel/QuickLinks.js
@@ -8,7 +8,7 @@ class QuickLinks extends Component {
             <div className="quicklinks-container">
                 <div className="row">
                     <div className="col-sm-12">
-                        <h1 className="quick-links-h1">Quick Recipe search</h1>
+                        <h1 className="quick-links-h1">Quick Recipe Categories</h1>
                     </div>
                 </div>
                 <div className="row">
@@ -23,67 +23,67 @@ class QuickLinks extends Component {
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Beef</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Vegitarian</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Fish</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Mexican</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Chocolate</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Fruit</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Indian</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Low-calorie Desert</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Breakfast</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Peanut Butter</p>
                                 </a>
                             </div>
                             <div className="slide-pic">
                                 <a href="#">
                                     <img src={samplepic} className="pic" alt="example" /><br />
-                                    <p className="category-title">Chicken</p>
+                                    <p className="category-title">Black Bean</p>
                                 </a>
                             </div>
                         </div>
