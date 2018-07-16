42ebc33c30916c53606548d8e7485d921b1f5b06
ashleynicole472 <asha.4720@gmail.com>
2018-07-16 10:43:18 -0700

    Added font awesome link, worked on search bar component

--- fitfoodie-recipesearch/src/Components/SearchBar/SearchBar.js
+++ fitfoodie-recipesearch/src/Components/SearchBar/SearchBar.js
@@ -4,8 +4,21 @@ import './SearchBar.css';
 class SearchBar extends Component {
     render(){
         return(
-            <div>
-                <h1>Within Search Bar</h1>
+            <div className="searchbar-container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <form className="navbar-form ">
+                            <div className="form-group">
+                                <div className="input-group">
+                                    <input className="form-control" type="text" placeholder="Search a recipe or by ingredient" />
+                                    <button className="btn btn-info input-group-addon">
+                                        <i className="fas fa-search"></i>
+                                  </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
