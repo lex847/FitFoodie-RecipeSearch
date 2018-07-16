aaf4dd02c62f31784b7ad668aaea067c822dd27a
ashleynicole472 <asha.4720@gmail.com>
2018-07-16 12:46:41 -0700

    Working on random recipes component

--- fitfoodie-recipesearch/src/Components/NavBar/NavBar.js
+++ fitfoodie-recipesearch/src/Components/NavBar/NavBar.js
@@ -31,7 +31,7 @@ class NavBar extends Component {
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Favorites</a>
+                                <a className="nav-link" href="#"><i className="far fa-heart heart-navbar"></i> Favorites</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Help</a>
--- fitfoodie-recipesearch/src/Components/RandomlySelectedRecipes/RandomRecipesSquares.js
+++ fitfoodie-recipesearch/src/Components/RandomlySelectedRecipes/RandomRecipesSquares.js
@@ -4,8 +4,52 @@ import './RandomRecipesSquares.css';
 class RandomRecipesSquares extends Component {
     render(){
         return(
-            <div>
-                <h1>Within Random Recipes Squares</h1>
+            <div className="random-recipes-container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Daily Suggestions:</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="row row-margin">
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="container">
+                            <img src="http://asvs.in/wp-content/uploads/2017/08/dummy.png" alt="Sample picture" />
+                            <button className="btn"><i className="far fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
